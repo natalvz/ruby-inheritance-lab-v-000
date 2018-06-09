1e6f3ca4902a3ad5e1e75c767a9651beba5da083
Natalia Castro <nat.alvz@gmail.com>
2018-06-09 21:06:25 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -1,12 +1,12 @@
 class User 
   attr_accessor :first_name, :last_name, :name
-  
+ 
   def initialize
     @first_name
     @last_name
   end
   
   def new 
-    KNOWLEDGE = []
+    @@knowledge = []
   end
 end
